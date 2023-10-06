3abb091ca31c5bfae69a96c9965f5e4f61c12c7b
Benjamin Lorenzo <benjalorenzo96@hotmail.com>
2023-10-06 17:40:49 -0300

    Actualización v12

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ import viewsRouter from './src/router/viewsRouter.js'; // Importa el viewsRouter
 import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
 import session from 'express-session'; // Importa express-session
+import passport from 'passport'; // Importa passport
+import GitHubStrategy from 'passport-github2'; // Importa GitHubStrategy
+import bcrypt from 'bcrypt'; // Importa bcrypt
+import User from './dao/models/userModel.js'; // Asegúrate de importar tu modelo de usuario
+import LocalStrategy from 'passport-local'; // Importa LocalStrategy si usas esta estrategia
 
 
 const app = express(); // Crear la instancia de Express
@@ -50,6 +55,100 @@ app.use(
     saveUninitialized: false,
   })
 );
+
+// Configuración de Passport para la estrategia local
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    async (email, password, done) => {
+      try {
+        // Buscar al usuario en la base de datos por su correo electrónico
+        const user = await User.findOne({ email });
+
+        // Si no se encuentra el usuario, devolver un mensaje de error
+        if (!user) {
+          return done(null, false, { message: 'Usuario no encontrado' });
+        }
+
+        // Verificar la contraseña
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        // Si la contraseña no coincide, devolver un mensaje de error
+        if (!passwordMatch) {
+          return done(null, false, { message: 'Contraseña incorrecta' });
+        }
+
+        // Si las credenciales son válidas, devolver el usuario autenticado
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+// Configurar Passport para la autenticación de GitHub
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: 'Iv1.f832426aac67f628', // Reemplaza con tu Client ID de GitHub
+      clientSecret: '397af14ae92d316d3f6e93c3e192604f0732c0bb', // Reemplaza con tu Client Secret de GitHub
+      callbackURL: 'http://localhost:8080/api/sessions/github/callback', // Reemplaza con la URL correcta
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // Verificar si el usuario ya existe en la base de datos
+        const existingUser = await User.findOne({ githubId: profile.id });
+
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+
+        // Si no existe, crea un nuevo usuario con la información de GitHub
+        const newUser = new User({
+          username: profile.username,
+          githubId: profile.id,
+          password: '', // Debe estar vacío para los usuarios de GitHub
+        });
+
+        await newUser.save();
+        return done(null, newUser);
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
+
+
+// Configuración de Passport para serialización y deserialización de usuarios
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id).exec();
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+// Ruta para la autenticación de GitHub
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', {
+    successRedirect: '/products', // Redirige a la vista de productos después de la autenticación
+    failureRedirect: '/login', // Redirige al formulario de inicio de sesión en caso de error
+  })
+);
+app.use(passport.initialize()); // Inicializa Passport
+app.use(passport.session()); // Habilita la persistencia de sesiones de Passport
 
 app.use('/api/users', usersRouter);
 app.use('/api/pets', petsRouter);
--- src/router/sessionsRouter.js
+++ src/router/sessionsRouter.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import passport from 'passport';
+import bcrypt from 'bcrypt';
 import User from '../../dao/models/userModel.js'; // Asegúrate de importar tu modelo de usuario
 
 const sessionsRouter = express.Router();
@@ -14,8 +16,18 @@ sessionsRouter.post('/register', async (req, res) => {
       return res.status(400).json({ error: 'El correo electrónico ya está en uso.' });
     }
 
-    // Crea un nuevo usuario
-    const newUser = new User({ first_name, last_name, email, age, password });
+    // Hash de la contraseña antes de guardarla
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Crea un nuevo usuario con la contraseña hasheada
+    const newUser = new User({
+      first_name,
+      last_name,
+      email,
+      age,
+      password: hashedPassword,
+    });
+
     await newUser.save();
 
     // Puedes agregar lógica adicional, como iniciar sesión automáticamente después del registro
@@ -26,37 +38,42 @@ sessionsRouter.post('/register', async (req, res) => {
   }
 });
 
-// Ruta para iniciar sesión
-sessionsRouter.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    // Verifica si el usuario existe en la base de datos
-    const user = await User.findOne({ email });
-
-    if (!user || user.password !== password) {
-      return res.status(401).json({ error: 'Credenciales inválidas.' });
+// Ruta para iniciar sesión con estrategia local (passport)
+sessionsRouter.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
     }
-
-    // Almacena el usuario en la sesión
-    req.session.user = user;
-
-    // Redirige al usuario a la vista de productos
-    res.redirect('/products');
-  } catch (error) {
-    res.status(500).json({ error: 'Error al iniciar sesión.' });
-  }
+    if (!user) {
+      // La autenticación ha fallado, redirige al formulario de inicio de sesión
+      return res.redirect('/login');
+    }
+    // La autenticación ha tenido éxito, inicia sesión en el usuario y redirige
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect('/products'); 
+    });
+  })(req, res, next);
 });
 
 // Ruta para cerrar sesión
 sessionsRouter.post('/logout', (req, res) => {
   // Destruye la sesión actual
-  req.session.destroy((err) => {
-    if (err) {
-      return res.status(500).json({ error: 'Error al cerrar sesión.' });
-    }
-    res.json({ message: 'Sesión cerrada correctamente.' });
-  });
+  req.logout();
+  res.json({ message: 'Sesión cerrada correctamente.' });
 });
 
+// Ruta para autenticación de GitHub
+sessionsRouter.get('/github', passport.authenticate('github'));
+
+// Ruta de retorno de GitHub después de la autenticación
+sessionsRouter.get('/github/callback', passport.authenticate('github', {
+  successRedirect: '/products', // Redirige al usuario a la vista de productos si la autenticación tiene éxito
+  failureRedirect: '/login',    // Redirige al usuario de nuevo al formulario de inicio de sesión si la autenticación falla
+}));
+
 export default sessionsRouter;
+
+
