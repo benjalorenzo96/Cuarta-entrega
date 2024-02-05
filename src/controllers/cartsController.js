8dd459fc17fe744c5875b8da9601a33ef067b1cc
Benjamin Lorenzo <benjalorenzo96@hotmail.com>
2024-02-05 13:59:40 -0300

    Actualización v29

--- src/controllers/cartsController.js
+++ src/controllers/cartsController.js
@@ -201,35 +201,52 @@ purchaseCart: async (req, res) => {
    * @description Agrega un producto al carrito, verificando la disponibilidad de stock y propiedad (solo para usuarios premium).
    */
   // Controlador para agregar un producto al carrito
-addToCart: async (req, res) => {
-  const productId = req.params.pid;
-  const quantity = parseInt(req.body.quantity);
+  addToCart: async (req, res) => {
+    console.log(req.params)
+const productId = req.params.pid;
+console.log("hola")
+console.log(productId)
+console.log(req.body)
+const quantity = parseInt(req.body.quantity);
+console.log(quantity)
+  
+    try {
+      console.log("Número de línea 209");
+      // Obtener información del producto
+      const product = await ProductDAO.getProductById(productId);
+      console.log("Número de línea 211");
+  
+      // Verificar propiedad del producto (solo para usuarios premium)
+      if (req.user.role === 'premium' && product.owner === req.user.email) {
+        return res.status(403).json({ error: 'No puedes agregar a tu carrito un producto que te pertenece' });
+      }
+  
+      // Verificar disponibilidad de stock
+      if (quantity > product.stock) {
+        return res.status(400).json({ error: 'No hay suficiente stock disponible para agregar la cantidad solicitada' });
+      }
+  
+      // Actualizar el carrito con el nuevo producto
+      console.log("Número de línea 224");
+      await updateCart(req.user.cartId, productId, quantity);
+      console.log("Número de línea 226");
+  
+      // Obtén el carrito actualizado y responde con éxito
+      const updatedCart = await Cart.findById(req.user.cartId);
+      console.log("Número de línea 231");
+  
+      res.status(200).json({ message: 'Producto agregado al carrito exitosamente', updatedCart });
+      console.log("hola")
 
-  try {
-    // Obtener información del producto
-    const product = await ProductDAO.getProductById(productId);
+    } catch (error) {
+      console.log("hola")
 
-    // Verificar propiedad del producto (solo para usuarios premium)
-    if (req.user.role === 'premium' && product.owner === req.user.email) {
-      return res.status(403).json({ error: 'No puedes agregar a tu carrito un producto que te pertenece' });
+      console.error(error);
+      console.log("hola")
+
+      res.status(500).json({ error: 'Error al agregar el producto al carrito' });
     }
-
-    // Verificar disponibilidad de stock
-    if (quantity > product.stock) {
-      return res.status(400).json({ error: 'No hay suficiente stock disponible para agregar la cantidad solicitada' });
-    }
-
-    // Actualizar el carrito con el nuevo producto
-    await updateCart(req.user.cartId, productId, quantity);
-
-    // Obtén el carrito actualizado y responde con éxito
-    const updateCart = await Cart.findById(req.user.cartId);
-    res.status(200).json({ message: 'Producto agregado al carrito exitosamente', updateCart });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error al agregar el producto al carrito' });
   }
-},
 }
 
 export default cartsController;
